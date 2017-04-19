172be0d32dd2aaa13add58390543498a7b315317
northcom <northcom@northcomdemac-mini.local>
2017-04-19 09:06:19 +0800

    Initial Commit

---
+++ AZPickViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AZPickViewDemo
+//
+//  Created by northcom on 17/4/19.
+//  Copyright © 2017年 安自鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AZPickViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AZPickViewDemo
+//
+//  Created by northcom on 17/4/19.
+//  Copyright © 2017年 安自鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
